306da7858da4ca292da72723b8198f5ba861eed6
Ralphie <ralphie94@pacbell.net>
2021-02-12 21:09:09 -0800

    started building out app.js

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import QuestionCard from "./components/QuestionCard";
+
+
+const App = () => {
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
+    return (
+      <div className="App">
+        <h1>Trivia</h1>
+        <button className="start" onClick={startTrivia}>
+          Start
+        </button>
+        <p className="score">Score:</p>
+        <p>Loading Questions...</p>
+        <QuestionCard />
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+      </div>
+    );
+}
+
+export default App;
